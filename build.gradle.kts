8fb4f2b2c8cbfe0a4219581ddd910781ed570489
Gabriel Konat <gabrielkonat@gmail.com>
2019-11-22 16:01:10 +0100

    Update gradle.config and Gradle wrapper version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.3.8"
-  id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.3.8"
-  id("org.metaborg.gradle.config.junit-testing") version "0.3.8"
+  id("org.metaborg.gradle.config.root-project") version "0.3.10"
+  id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.3.10"
+  id("org.metaborg.gradle.config.junit-testing") version "0.3.10"
   id("org.metaborg.gitonium") version "0.1.2"
   kotlin("jvm") version "1.3.21"
   `kotlin-dsl`
