78e3921d8a4f5d57d808c9f88bd9f57d43a93e00
Gabriel Konat <gabrielkonat@gmail.com>
2020-04-22 15:41:18 +0200

    Update Gradle plugin versions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,13 +2,6 @@ rootProject.name = "coronium"
 
 pluginManagement {
   repositories {
-    // Get plugins from artifacts.metaborg.org, first.
-    maven("https://artifacts.metaborg.org/content/repositories/releases/")
-    maven("https://artifacts.metaborg.org/content/repositories/snapshots/")
-    // Required by several Gradle plugins (Maven central).
-    maven("https://artifacts.metaborg.org/content/repositories/central/") // Maven central mirror.
-    mavenCentral() // Maven central as backup.
-    // Get plugins from Gradle plugin portal.
-    gradlePluginPortal()
+    maven("https://artifacts.metaborg.org/content/groups/public/")
   }
 }
