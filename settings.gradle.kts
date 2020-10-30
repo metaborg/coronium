5c76ad53c44dc2145e0e66efc88c727038c6c809
Gabriel Konat <gabrielkonat@gmail.com>
2020-10-30 11:33:34 +0100

    Gradle >=6 compatibility.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,19 @@ if(gradle.parent == null) {
   // We split the build up into one main composite build in the 'plugin' directory, because it builds Gradle plugins,
   // which we want to test. Gradle plugins are not directly available in a multi-project build, therefore a separate
   // composite build is required.
-  includeBuild("plugin")
+  includeBuildWithName("plugin", "coronium")
   // Included builds listed below can use the Gradle plugins built in 'plugin'.
-  includeBuild("example")
+  includeBuildWithName("example", "coronium.example")
+}
+
+fun includeBuildWithName(dir: String, name: String) {
+  includeBuild(dir) {
+    try {
+      ConfigurableIncludedBuild::class.java
+        .getDeclaredMethod("setName", String::class.java)
+        .invoke(this, name)
+    } catch(e: NoSuchMethodException) {
+      // Running Gradle < 6, no need to set the name, ignore.
+    }
+  }
 }
