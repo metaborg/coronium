ed96a7517f2f55c420b40e2bceb47f6eaf296ca6
Gabriel Konat <gabrielkonat@gmail.com>
2021-07-20 15:49:44 +0200

    Update gradle.config to 0.4.6.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.4.4"
+  id("org.metaborg.gradle.config.root-project") version "0.4.6"
   id("org.metaborg.gradle.config.kotlin-gradle-plugin") version "0.4.4"
   id("org.metaborg.gradle.config.junit-testing") version "0.4.4"
   id("org.metaborg.gitonium") version "0.1.4"
