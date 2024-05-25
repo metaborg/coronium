57a137e46cde191cd6c86bd482cce9f1456abca0
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-26 00:14:53 +0200

    Simplify nested composite builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,16 +7,12 @@ pluginManagement {
 }
 
 
-// Only include composite builds when this is the root project (it has no parent). Otherwise, the parent project will
-// include these composite builds, as IntelliJ does not support nested composite builds.
-if (gradle.parent == null) {
-    // We split the build up into one main composite build in the 'plugin' directory, because it builds Gradle plugins,
-    // which we want to test. Gradle plugins are not directly available in a multi-project build, therefore a separate
-    // composite build is required.
-    includeBuildWithName("plugin", "coronium")
-    // Included builds listed below can use the Gradle plugins built in 'plugin'.
-    includeBuildWithName("example", "coronium.example")
-}
+// We split the build up into one main composite build in the 'plugin' directory, because it builds Gradle plugins,
+// which we want to test. Gradle plugins are not directly available in a multi-project build, therefore a separate
+// composite build is required.
+includeBuildWithName("plugin", "coronium")
+// Included builds listed below can use the Gradle plugins built in 'plugin'.
+includeBuildWithName("example", "coronium.example")
 
 fun includeBuildWithName(dir: String, name: String) {
     includeBuild(dir) {
